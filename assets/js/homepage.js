4f3ea4d5e51992c6c56d6b0367a456b0870dde2b
jeremyrice98 <ricefamily1003@gmail.com>
2020-06-13 13:12:32 -0600

    error corrections

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,31 +2,34 @@ var searchEl = document.getElementById('search-button');
 var searchHistory = JSON.parse(localStorage.getItem('monster')) || [];
 var inputEl = document.getElementById('monster-input');
 var monsterContainerEl = document.getElementById('monster-container')
+var monsterSmContainerEl = document.getElementById('monster-container-sm')
 
 //initialize search for given city name
-searchEl.addEventListener('click',function() {
+searchEl.addEventListener('click', function() {
     var searchTerm = inputEl.value;
     getMonster(searchTerm);
     searchHistory.push(searchTerm);
-    localStorage.setItem('monster',JSON.stringify(searchHistory));
+    localStorage.setItem('monster', JSON.stringify(searchHistory));
 });
 
 function getMonster(name) {
     var queryURL = 'https://www.dnd5eapi.co/api/monsters/' + name;
     axios.get(queryURL)
-    .then (function(response) {
-        var monsterEl = document.createElement ('ul');
-        monsterEl.classList = 'card list-item';
-        monsterEl.setAttribute ('id', 'monster-container');
+        .then(function(response) {
+            var monsterEl = document.createElement('ul');
+            monsterEl.classList = 'card';
+            // monsterEl.setAttribute('id', 'monster-container');
 
-        var monsterName = document.createElement('li');
-        monsterName.innerText = 'Name: ' + response.data.name;
-        monsterEl.appendChild(monsterName);
+            var monsterName = document.createElement('li');
+            monsterName.innerText = 'Name: ' + response.data.name;
+            monsterEl.appendChild(monsterName);
 
-        var monsterHP = document.createElement('li');
-        monsterHP.innerText = 'HP: ' + response.data.hit_points;
-        monsterEl.appendChild(monsterHP);
+            var monsterHP = document.createElement('li');
+            monsterHP.innerText = 'HP: ' + response.data.hit_points;
+            monsterEl.appendChild(monsterHP);
 
-        monsterContainerEl.appendChild(monsterEl);
-    });
-}   
+            monsterSmContainerEl.appendChild(monsterEl);
+            monsterContainerEl.appendChild(monsterEl);
+
+        });
+}
