61b3de85fd88d283dd6db81e1fee474a6623d0c1
Wiretys <wiretys@gmail.com>
2020-06-15 17:45:18 -0400

    update form

--- assets/js/rexjs.js
+++ assets/js/rexjs.js
@@ -3,7 +3,31 @@
 $("#addplayerSubmit").click(function () {
     var playerName = $("#playerName").val();
     var playerClass = $("#playerClass").val();
-    var arrayForm = [playerName, playerClass];
+    var playerRace = $("#playerRace").val();
+    var healthPoints = $("#healthPoints").val();
+    var strength = $("#strength").val();
+    var dexterity = $("#dexterity").val();
+    var intelligence = $("#intelligence").val();
+    var wisdom = $("#wisdom").val();
+    var constitution = $("#constitution").val();
+    var charisma = $("#charisma").val();
+    var weaponsOne = $("#weaponsOne").val();
+    var weaponsTwo = $("#weaponsTwo").val();
+    var weaponsThree = $("#weaponsThree").val();
+    var weaponsFour = $("#weaponsFour").val();
+    var weaponsFive = $("#weaponsFive").val();
+    var weaponsSix = $("#weaponsSix").val();
+    var weaponsSeven = $("#weaponsSeven").val();
+    var weaponsEight = $("#weaponsEight").val();
+    var spellOne = $("#spellOne").val();
+    var spellTwo = $("#spellTwo").val();
+    var spellThree = $("#spellThree").val();
+    var spellFour = $("#spellFour").val();
+    var statusEffectOne = $("#statusEffectOne").val();
+    var statusEffectTwo = $("#statusEffectTwo").val();
+    var statusEffectThree = $("#statusEffectThree").val();
+
+    var arrayForm = [playerName, playerClass,playerRace,healthPoints,strength,dexterity,intelligence,wisdom,constitution,charisma,weaponsOne,weaponsTwo,weaponsThree,weaponsFour,weaponsFive,weaponsSix,weaponsSeven,weaponsEight,spellOne,spellTwo,spellThree,spellFour,statusEffectOne,statusEffectTwo,statusEffectThree,statusEffectFour];
 
     // var texttoEnter = arrayForm;
     var texttoEnterJSON = JSON.stringify(arrayForm);
@@ -19,3 +43,37 @@ $("#addplayerSubmit").click(function () {
 
 // });
 
+// johns code
+
+// var searchEl = document.getElementById('search-button');
+// var searchHistory = JSON.parse(localStorage.getItem('monster')) || [];
+// var inputEl = document.getElementById('monster-input');
+// var monsterContainerEl = document.getElementById('monster-container')
+
+// //initialize search for given city name
+// searchEl.addEventListener('click',function() {
+//     var searchTerm = inputEl.value;
+//     getMonster(searchTerm);
+//     searchHistory.push(searchTerm);
+//     localStorage.setItem('monster',JSON.stringify(searchHistory));
+// });
+
+// function getMonster(name) {
+//     var queryURL = 'https://www.dnd5eapi.co/api/equipment/equipment_category/weapon';
+//     axios.get(queryURL)
+//     .then (function(response) {
+//         var monsterEl = document.createElement ('ul');
+//         monsterEl.classList = 'card list-item';
+//         monsterEl.setAttribute ('id', 'monster-container');
+
+//         var monsterName = document.createElement('li');
+//         monsterName.innerText = 'Name: ' + response.data.name;
+//         monsterEl.appendChild(monsterName);
+
+//         var monsterHP = document.createElement('li');
+//         monsterHP.innerText = 'HP: ' + response.data.hit_points;
+//         monsterEl.appendChild(monsterHP);
+
+//         monsterContainerEl.appendChild(monsterEl);
+//     });
+// }   
