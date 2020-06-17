fe0bafbe08dc5b7f7da94bcc1c89bac7ef87ec39
grahamgibbs <63676324+grahamgibbs@users.noreply.github.com>
2020-06-17 13:20:52 -0600

    added card

--- assets/js/name-gen.js
+++ assets/js/name-gen.js
@@ -8,6 +8,26 @@ nameEl.addEventListener('click', function () {
     localStorage.setItem('randomName', JSON.stringify(nameGen));
 })
 
+$(function(){
+	var minlen = $("#minlen").text(),
+		maxlen = $("#maxlen").text();
+		$("#min").val(minlen);
+		$("#max").val(maxlen);
+	$("#slider-range").slider({
+		range: true,
+		min: 2,
+		max: 40,
+		values: [minlen, maxlen],
+		step: 1,
+		slide: function(event, ui){
+			$("#amount").html(ui.values[0] +" - "+ ui.values[1]);
+			$("#min").val(ui.values[0]);
+			$("#max").val(ui.values[1]);
+		}
+	});
+	$("#amount").html($("#slider-range").slider("values", 0) +" - "+ $("#slider-range").slider("values", 1));
+});
+
 function randomName () {
     axios.get(queryURL2)
         .then(response => {
