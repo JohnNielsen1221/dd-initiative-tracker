9cfdad864078746361e4e28954da4bdb98e9d4a6
grahamgibbs <63676324+grahamgibbs@users.noreply.github.com>
2020-06-17 22:33:08 -0600

    minor change

--- assets/js/name-gen.js
+++ assets/js/name-gen.js
@@ -2,15 +2,16 @@ var queryURL2 = 'https://www.uzby.com/api.php?min=4&max=9';
 
 
 
-function options 
-    method: 'GET',
-    mode: 'no-cors'
+
 
 
 function getRandomName() {
     axios.get(queryURL2)
-        .then(function (response) {
+        .then(function (_response) {
             var setRandomName = document.getElementById('random-name');
+            
+            return(getRandomName)
+        M.Tooltip.init(setRandomName);   
         })
 }
 function setRandomName() {
