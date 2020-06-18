7618c87dec070d352900d79e47372ddd2de23cbc
grahamgibbs <63676324+grahamgibbs@users.noreply.github.com>
2020-06-18 12:32:00 -0600

    trying fetch

--- assets/js/name-gen.js
+++ assets/js/name-gen.js
@@ -1,24 +1,31 @@
 var queryURL2 = 'https://www.uzby.com/api.php?min=4&max=9';
 var proxyurl = "https://cors-anywhere.herokuapp.com/";
-var setRandomName = document.getElementById('random-name');
+
 var generateName = document.getElementById('generate');
 
 
+    
+
+    
     generateName.addEventListener('click', function() {
         var getName = getRandomName.value;
-        getRandomName(getName); 
+        setRandomName(getName); 
+    });
+
+    fetch(proxyurl + queryURL2)
+    .then(function (response) {
+        response.queryURL2;
+        var setRandomName = document.getElementById('random-name');
+        var newName = response.queryURL2;
+        
+console.log(newName);
     });
 
 
-function getRandomName() {
-    axios.get(proxyurl + queryURL2)
-        .then(function (response) {
-            var setRandomName = document.getElementById('random-name');
-        })
-};
-console.log(getRandomName);
 
 function setRandomName() {
     document.getElementById('random-name').innerText = getRandomName();
 };
 
+
+    
