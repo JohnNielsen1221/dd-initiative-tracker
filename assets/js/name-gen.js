de6d8497925467a1e25dcee0a73ed4445af50c7a
grahamgibbs <63676324+grahamgibbs@users.noreply.github.com>
2020-06-17 22:10:13 -0600

    updated

--- assets/js/name-gen.js
+++ assets/js/name-gen.js
@@ -2,10 +2,13 @@ var queryURL2 = 'https://www.uzby.com/api.php?min=4&max=9';
 
 
 
+function options 
+    method: 'GET',
+    mode: 'no-cors'
 
 
 function getRandomName() {
-    axios.get(queryURL2 + name)
+    axios.get(queryURL2)
         .then(function (response) {
             var setRandomName = document.getElementById('random-name');
         })
